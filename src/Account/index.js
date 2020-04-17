826c031135639aa00208e7d4bd5ede0d255d4026
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-04-17 15:31:13 -0500

    i think working but console is loggin null going to see in next step

--- src/Account/index.js
+++ src/Account/index.js
@@ -2,17 +2,37 @@ import React, { Component } from 'react';
 
 
 class Account extends Component {
+  constructor() {
+    super()
+    this.ref1 = React.createRef()
+    this.state = {
+      balance: 0
+    }
+  }
   render() {
+    console.log(this.ref1);
     return (
       <div className="account">
         <h2>{this.props.name}</h2>
         <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
+        <input 
+          type="text"
+          ref={this.ref1}
+          placeholder="enter an amount" />
         <input type="button" value="Deposit" />
         <input type="button" value="Withdraw" />
       </div>
     )
   }
 }
+
+
+
+
+
+
+
+
+
 
 export default Account;
