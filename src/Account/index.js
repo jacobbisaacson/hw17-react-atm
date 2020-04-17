ddfa9204a6ee77f1c62fd6f526e91a4888482ddc
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-04-17 15:46:43 -0500

    stupid didnt read the terminal quick fix with teh missing equals sign and needed current value to parsefloat properly

--- src/Account/index.js
+++ src/Account/index.js
@@ -11,14 +11,14 @@ class Account extends Component {
   }
 
   handleClick = (event) => {
-    let withdrawMoney = parseFloat(this.ref1.value)
+    let withdrawMoney = parseFloat(this.ref1.current.value)
     console.log(withdrawMoney);
     // console.log("handling click");
     // console.log(event.target);
     let balance = this.state.balance
-    if(event.target.value == "Deposit") {
+    if(event.target.value === "Deposit") {
       balance += withdrawMoney
-    } else if(event.target.value == "Withdraw") {
+    } else if(event.target.value === "Withdraw") {
       balance -= withdrawMoney
     }
 
